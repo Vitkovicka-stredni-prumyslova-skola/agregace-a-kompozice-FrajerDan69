0dd331761f7ca80fdeb4094a5310c15a5c87d32d
FrajerDan69 <98973@student.vitkovickastredni.cz>
2026-02-03 09:40:24 +0100

    jj

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd2a6b53a1e6c29e4049fa27c67cdee72dd9fa34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a466aaf519d55c5ede5d3a841e317d67bc57480")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
